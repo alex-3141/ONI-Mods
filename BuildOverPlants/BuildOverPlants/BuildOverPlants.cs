54215b06bbc4f8d9dbe795fb391d1d974476d6e5
Soup <root@localhost.localdomain>
2019-08-16 01:29:04 +0930

    Added BuildoverPlants

---
+++ BuildOverPlants/BuildOverPlants/BuildOverPlants.cs
@@ -0,0 +1,68 @@
+﻿using Harmony;
+using UnityEngine;
+
+/*
+ * TODO: Improve compatibility with sandbox mode
+ * 
+ */
+
+
+namespace BuildOverPlants
+{
+	public static class BuildOverPlants
+	{
+		public static class BuildOverPlants_OnLoad
+		{
+			public static void OnLoad()
+			{
+				Debug.Log("Build Over Plants Mod Loaded");
+			}
+		}
+
+		// Patch BuildingDef.IsAreaClear to allow placement of buildings over plants
+		[HarmonyPatch(typeof(BuildingDef))]
+		[HarmonyPatch("IsAreaClear")]
+		public static class BuildOverPlants_Patch_BuildingDef_IsAreaClear
+		{
+			public static void Prefix(ref BuildingDef __instance, ref int cell, ref Orientation orientation, ref ObjectLayer layer)
+			{
+				for (int i = 0; i < __instance.PlacementOffsets.Length; i++) {
+					CellOffset offset = __instance.PlacementOffsets [i];
+					CellOffset rotatedCellOffset = Rotatable.GetRotatedCellOffset (offset, orientation);
+					int num = Grid.OffsetCell (cell, rotatedCellOffset);
+					GameObject gameObject = Grid.Objects [num, (int)layer];
+					if (gameObject != null) { 
+						if (gameObject.GetComponent<Uprootable> () != null) {
+							// Here I move the plant object to the Plants object layer (It's on the building layer by default)
+							// This allows the building to be placed without overwriting the plant object and without the plant getting in the way
+							Grid.Objects [num, (int)ObjectLayer.Plants] = gameObject;
+							Grid.Objects [num, (int)ObjectLayer.Building] = null;
+						}
+					}
+				}
+			}
+		}
+
+		// Patch Constructable.PlaceDiggables to mark plants for uprooting when they get in the way
+		[HarmonyPatch(typeof(Constructable))]
+		[HarmonyPatch("PlaceDiggables")]
+		public static class BuildOverPlants_Patch_Constructable_PlaceDiggables
+		{
+			public static void Postfix(ref Constructable __instance, ref Building ___building)
+			{
+				PrioritySetting masterPriority = __instance.GetComponent<Prioritizable> ().GetMasterPriority ();
+				___building.RunOnArea (delegate (int offset_cell) {
+					GameObject gameObject = Grid.Objects [offset_cell, (int)ObjectLayer.Plants];
+					if (gameObject != null) {
+						// This component should always exist but lets check it just in case
+						if (gameObject.GetComponent<Uprootable> () != null) {
+							gameObject.GetComponent<Uprootable> ().MarkForUproot (true);
+							gameObject.GetComponent<Prioritizable> ().SetMasterPriority (masterPriority);
+
+						}
+					}
+				});
+			}
+		}
+	}
+}
---
+++ BuildOverPlants/BuildOverPlants/ModInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildOverPlants
+{
+	public static class ModInfo
+	{
+		public static string Name = "Build Over Plants";
+		public static string Version = System.DateTime.UtcNow.ToString("yyyy.MM.dd");
+	}
+}
